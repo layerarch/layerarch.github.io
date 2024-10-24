f70fd8ec0037315814e63caf7daf6934aa10ddba
mmacneill3892 <mmacneill@layerarch.ca>
2024-10-24 02:21:21 -0400

    fixed header

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -45,6 +45,10 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+// var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+// var collapseList = collapseElementList.map(function (collapseEl) {
+//   return new bootstrap.Collapse(collapseEl)
+// })
 
 
 // document.addEventListener("DOMContentLoaded", function() {
